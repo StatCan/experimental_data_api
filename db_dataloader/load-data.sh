707b91645d121582d5a45d4f14f22b42c259d876
Laurent Goderre <laurent.goderre@gmail.com>
2019-03-13 12:06:59 -0400

    Prevent loading data when data already exists

--- db_dataloader/load-data.sh
+++ db_dataloader/load-data.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
-./load-population-estimates.sh \
-&& ./load-birth-death.sh
+if [ "$(psql -h "$PGHOST" -d "$POSTGRES_DB" -U postgres -At -c 'SELECT COUNT(*) FROM observations')" -eq 0 ]; then
+  ./load-population-estimates.sh \
+  && ./load-birth-death.sh
+fi
