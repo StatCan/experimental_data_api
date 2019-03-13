eaf794e6d0f458f144979f326f5fc738083521e8
Laurent Goderre <laurent.goderre@gmail.com>
2019-03-12 22:26:05 -0400

    Added data loader for population estimates

---
+++ db_dataloader/load-data.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+./load-population-estimates.sh
---
+++ db_dataloader/wait-for.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# wait-for-postgres.sh
+
+set -e
+
+cmd="$@"
+
+until PGPASSWORD=$POSTGRES_PASSWORD psql -h "$PGHOST" -U "postgres" -c '\q'; do
+  >&2 echo "Postgres is unavailable - sleeping"
+  sleep 1
+done
+
+>&2 echo "Postgres is up - executing command"
+exec $cmd
