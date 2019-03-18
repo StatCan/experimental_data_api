11be35430bc537241ed4d8fa1ce3b2a08b9164be
Laurent Goderre <laurent.goderre@gmail.com>
2019-03-18 15:08:41 -0400

    Supress postgres waiting for boot errors

--- db_dataloader/wait-for.sh
+++ db_dataloader/wait-for.sh
@@ -5,7 +5,7 @@ set -e
 
 cmd="$@"
 
-until PGPASSWORD=$POSTGRES_PASSWORD psql -h "$PGHOST" -U "postgres" -c '\q'; do
+until PGPASSWORD=$POSTGRES_PASSWORD psql -h "$PGHOST" -U "postgres" -c '\q' 2> /dev/null; do
   >&2 echo "Postgres is unavailable - sleeping"
   sleep 1
 done
