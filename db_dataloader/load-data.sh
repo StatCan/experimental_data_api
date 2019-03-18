480a535efdf3e2bd7eec55678f434cf5c7a8d905
Laurent Goderre <laurent.goderre@gmail.com>
2019-03-18 16:08:19 -0400

    Added migration data

--- db_dataloader/load-data.sh
+++ db_dataloader/load-data.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 if [ "$(psql -At -c 'SELECT COUNT(*) FROM observations')" -eq 0 ]; then
   ./load-population-estimates.sh \
-  && ./load-birth-death.sh
+  && ./load-birth-death.sh \
+  && ./load-migrants.sh
 fi
