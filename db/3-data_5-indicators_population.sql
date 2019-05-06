5035d1ddb0a635dde54a155af2ac7e38f9cad137
Laurent Goderre <laurent.goderre@gmail.com>
2019-05-06 13:25:47 -0400

    Split the indicators db insert into multiple files

--- db/3-data_5-indicators_population.sql
+++ db/3-data_5-indicators_population.sql
@@ -81,31 +81,3 @@ VALUES (
   (SELECT id FROM subjects WHERE name = 'population_and_demography')
 );
 END; $$;
-
-INSERT INTO indicators (name, frequency_id, title)
-VALUES
-(
-  'airFares', (SELECT id FROM frequencies WHERE name = 'quarterly'),
-  hstore(ARRAY[['en', ''], ['fr', '']])
-);
-
-DO $$
-DECLARE
-  ind varchar = 'airFares';
-  dim varchar;
-BEGIN
-FOREACH dim IN ARRAY ARRAY['geographicArea', 'flightSector', 'airFareTypeGroup']
-LOOP
-  INSERT INTO indicator_dimensions (indicator_id, dimension_id)
-  VALUES (
-    (SELECT id FROM indicators WHERE name = ind),
-    (SELECT id FROM dimensions WHERE name = dim)
-  );
-END LOOP;
-
-INSERT INTO indicator_subjects (indicator_id, subject_id)
-VALUES (
-  (SELECT id FROM indicators WHERE name = ind),
-  (SELECT id FROM subjects WHERE name = 'transportation')
-);
-END; $$
