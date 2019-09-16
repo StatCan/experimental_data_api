083cfc81bac39f66ac552eb042381cf8a74b188d
Laurent Goderre <laurent.goderre@gmail.com>
2019-09-16 14:46:19 +0000

    Added scaffolding for unit tests

---
+++ api/test/index.js
@@ -0,0 +1,9 @@
+/* eslint-env mocha */
+/* eslint "node/no-unpublished-require": 0 */
+const assert = require('assert');
+
+describe('Tests', () => {
+	it('should pass', async () => {
+		assert.strictEqual(true, true);
+	});
+});
