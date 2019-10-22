d3c9a3bb1940096a38e9cea08a289e31ca89fa8c
Laurent Goderre <laurent.goderre@gmail.com>
2019-10-22 14:45:39 +0000

    Added unit tests for the indicators model

--- api/test/index.js
+++ api/test/index.js
@@ -1,9 +1,1 @@
-/* eslint-env mocha */
-/* eslint "node/no-unpublished-require": 0 */
-const assert = require('assert');
-
-describe('Tests', () => {
-	it('should pass', async () => {
-		assert.strictEqual(true, true);
-	});
-});
+require('./indicators');
---
+++ api/test/indicators.js
@@ -0,0 +1,354 @@
+/* eslint-env mocha */
+/* eslint "node/no-unpublished-require": 0 */
+const assert = require('assert');
+
+describe('Indicators', () => {
+	describe('Model', () => {
+		const indicators = require('../endpoints/indicators/model');
+
+		describe('`list` function', () => {
+			it('should return an object containing list of indicators and total count', async () => {
+				return new Promise(async (resolve, reject) => {
+					const list = await indicators.list(0, 10).catch(reject);
+					try {
+						assert.strictEqual(typeof list, 'object');
+						assert.strictEqual(list.length, 2);
+						assert.strictEqual(list.list.length, 2);
+						resolve();
+					} catch (e) {
+						reject(e);
+					}
+				});
+			});
+
+			it('should return the same object as the `get` function', async () => {
+				return new Promise(async (resolve, reject) => {
+					const list = await indicators.list(0, 10).catch(reject);
+					const indicator = await indicators.get(list.list[0].id).catch(reject);
+					try {
+						assert.strictEqual(JSON.stringify(list.list[0]), JSON.stringify(indicator));
+						resolve();
+					} catch (e) {
+						reject(e);
+					}
+				});
+			});
+
+			it('should offset the list by the `start` argument', async () => {
+				return new Promise(async (resolve, reject) => {
+					const list = await indicators.list(1, 10).catch(reject);
+					try {
+						assert.strictEqual(list.list[0].id, 'indicator2');
+						resolve();
+					} catch (e) {
+						reject(e);
+					}
+				});
+			});
+
+			it('should limit the list to the `count` argument', async () => {
+				return new Promise(async (resolve, reject) => {
+					const list = await indicators.list(0, 1).catch(reject);
+					try {
+						assert.strictEqual(list.list.length, 1);
+						resolve();
+					} catch (e) {
+						reject(e);
+					}
+				});
+			});
+
+			describe('Sorting', () => {
+				it.skip('should sort by date modified in descending order by default');
+				it.skip('should sort by date modified in descending order when sorting is set to `dateModified` and order is undefined');
+				it.skip('should sort by date modified in descending order when sorting is set to `dateModified` and order is set to `descending`');
+				it.skip('should sort by date modified in ascending order when sorting is set to `dateModified` and order is set to `ascending`');
+				it.skip('should sort by name in ascending alphabetical order when sorting is set to `name` and order is undefined');
+				it.skip('should sort by name in ascending alphabetical order when sorting is set to `name` and order is set to `ascending`');
+				it.skip('should sort by name in descending alphabetical order when sorting is set to `name` and order is set to `descending`');
+			});
+
+			describe('Filtering', () => {
+				it.skip('should filter indicators by subject');
+			});
+		});
+
+		describe('`isValid` function', () => {
+			it('should return true for a valid indicator id', () => {
+				assert.strictEqual(indicators.isValid('test_indicator-id1'), true);
+			});
+
+			it('should return true for a valid indicator id', () => {
+				assert.strictEqual(indicators.isValid('%invalid-id'), false);
+			});
+		});
+
+		describe('`exists` function', () => {
+			it('should return `true` for existing indicator', async () => {
+				return new Promise(async (resolve, reject) => {
+					const exists = await indicators.exists('indicator2').catch(reject);
+					try {
+						assert.strictEqual(exists, true);
+						resolve();
+					} catch (e) {
+						reject(e);
+					}
+				});
+			});
+
+			it('should return `false` for non-existing indicator', async () => {
+				return new Promise(async (resolve, reject) => {
+					const exists = await indicators.exists('indicator0').catch(reject);
+					try {
+						assert.strictEqual(exists, false);
+						resolve();
+					} catch (e) {
+						reject(e);
+					}
+				});
+			});
+		});
+
+		describe('`get` function', () => {
+			it('should return an indicator object', async () => {
+				return new Promise(async (resolve, reject) => {
+					const indicator = await indicators.get('indicator2').catch(reject);
+					try {
+						assert.strictEqual(indicator.type, 'indicator');
+						assert.strictEqual(indicator.id, 'indicator2');
+						resolve();
+					} catch (e) {
+						reject(e);
+					}
+				});
+			});
+
+			it('should return `undefined` for non-existing indicator', async () => {
+				return new Promise(async (resolve, reject) => {
+					const indicator = await indicators.get('indicator0').catch(reject);
+					try {
+						assert.strictEqual(indicator, undefined);
+						resolve();
+					} catch (e) {
+						reject(e);
+					}
+				});
+			});
+		});
+
+		describe('`listObservations` function', () => {
+			const observations = require('../endpoints/observations/model');
+
+			it('should return an object containing list of observations for the indicators and total count', async () => {
+				return new Promise(async (resolve, reject) => {
+					const indicator = 'indicator1';
+					const observationsList = await indicators.listObservations(indicator, 0, 10).catch(reject);
+					try {
+						assert.strictEqual(observationsList.length, 7);
+						for (const {type, attributes: {indicator: observationIndicator}} of observationsList.list) {
+							assert.strictEqual(type, 'observation');
+							assert.strictEqual(indicator, observationIndicator);
+						}
+						resolve();
+					} catch (e) {
+						reject(e);
+					}
+				});
+			});
+
+			it('should return the same list as the observations model `list` function', async () => {
+				return new Promise(async (resolve, reject) => {
+					const indicator = 'indicator1';
+					const indicatorObservations = await indicators.listObservations(indicator, 0, 10).catch(reject);
+					const indicatorObservations2 = await observations.list(0, 10, undefined, {indicator}).catch(reject);
+
+					try {
+						assert.strictEqual(JSON.stringify(indicatorObservations), JSON.stringify(indicatorObservations2));
+						resolve();
+					} catch (e) {
+						reject(e);
+					}
+				});
+			});
+
+			it('should not error for non-existing indicator', async () => {
+				return new Promise(async (resolve, reject) => {
+					const observationsList = await indicators.listObservations('indicator0', 0, 10).catch(reject);
+					try {
+						assert.strictEqual(JSON.stringify(observationsList), '{"length":0,"list":[]}');
+						resolve();
+					} catch (e) {
+						reject(e);
+					}
+				});
+			});
+
+			it('should offset the list by the `start` argument', async () => {
+				return new Promise(async (resolve, reject) => {
+					const observations1 = await indicators.listObservations('indicator1', 1, 10).catch(reject);
+					const observations2 = await indicators.listObservations('indicator1', 0, 10).catch(reject);
+					try {
+						assert.strictEqual(observations1.list[0].id, observations2.list[1].id);
+						resolve();
+					} catch (e) {
+						reject(e);
+					}
+				});
+			});
+
+			it('should limit the list to the `count` argument', async () => {
+				return new Promise(async (resolve, reject) => {
+					const observationsList = await indicators.listObservations('indicator1', 0, 1).catch(reject);
+					try {
+						assert.strictEqual(observationsList.list.length, 1);
+						resolve();
+					} catch (e) {
+						reject(e);
+					}
+				});
+			});
+
+			describe('Sorting', () => {
+				it.skip('should sort by date modified in descending order by default');
+				it.skip('should sort by date modified in descending order when sorting is set to `dateModified` and order is undefined');
+				it.skip('should sort by date modified in descending order when sorting is set to `dateModified` and order is set to `descending`');
+				it.skip('should sort by date modified in ascending order when sorting is set to `dateModified` and order is set to `ascending`');
+				it.skip('should sort by reference period in descending order when sorting is set to `referencePeriod` and order is undefined');
+				it.skip('should sort by reference period in descending order when sorting is set to `referencePeriod` and order is set to `descending`');
+				it.skip('should sort by reference period in ascending order when sorting is set to `referencePeriod` and order is set to `ascending`');
+			});
+
+			describe('Filtering', () => {
+				it('should send filtering options to the observation `list` function', async () => {
+					return new Promise(async (resolve, reject) => {
+						const noFilter = await indicators.listObservations('indicator1', 0, 10).catch(reject);
+						const indicatorObservations = await indicators.listObservations('indicator1', 0, 10, undefined, {dimensions: {sex: ['female']}}).catch(reject);
+						const observationsList = await observations.list(0, 10, undefined, {indicator: 'indicator1', dimensions: {sex: ['female']}}).catch(reject);
+						try {
+							assert.notStrictEqual(JSON.stringify(noFilter), JSON.stringify(indicatorObservations));
+							assert.strictEqual(JSON.stringify(indicatorObservations), JSON.stringify(observationsList));
+							resolve();
+						} catch (e) {
+							reject(e);
+						}
+					});
+				});
+			});
+		});
+
+		describe('`listTimeseries` function', () => {
+			const timeseries = require('../endpoints/timeseries/model');
+
+			it('should return an object containing list of observations for the indicators and total count', async () => {
+				return new Promise(async (resolve, reject) => {
+					const indicator = 'indicator1';
+					const timeseriesList = await indicators.listTimeseries(indicator, 0, 10).catch(reject);
+					try {
+						assert.strictEqual(timeseriesList.length, 2);
+						for (const {type, attributes: {indicator: observationIndicator}} of timeseriesList.list) {
+							assert.strictEqual(type, 'timeseries');
+							assert.strictEqual(indicator, observationIndicator);
+						}
+						resolve();
+					} catch (e) {
+						reject(e);
+					}
+				});
+			});
+
+			it('should return the same list as the observations model `list` function', async () => {
+				return new Promise(async (resolve, reject) => {
+					const indicator = 'indicator1';
+					const indicatorTimeseries = await indicators.listTimeseries(indicator, 0, 10).catch(reject);
+					const indicatorTimeseries2 = await timeseries.list(0, 10, undefined, {indicator}).catch(reject);
+
+					try {
+						assert.strictEqual(JSON.stringify(indicatorTimeseries), JSON.stringify(indicatorTimeseries2));
+						resolve();
+					} catch (e) {
+						reject(e);
+					}
+				});
+			});
+
+			it('should not error for non-existing indicator', async () => {
+				return new Promise(async (resolve, reject) => {
+					const timeseriesList = await indicators.listTimeseries('indicator0', 0, 10).catch(reject);
+					try {
+						assert.strictEqual(JSON.stringify(timeseriesList), '{"length":0,"list":[]}');
+						resolve();
+					} catch (e) {
+						reject(e);
+					}
+				});
+			});
+
+			it('should offset the list by the `start` argument', async () => {
+				return new Promise(async (resolve, reject) => {
+					const timeseries1 = await indicators.listTimeseries('indicator1', 1, 10).catch(reject);
+					const timeseries2 = await indicators.listTimeseries('indicator1', 0, 10).catch(reject);
+					try {
+						assert.strictEqual(timeseries1.list[0].id, timeseries2.list[1].id);
+						resolve();
+					} catch (e) {
+						reject(e);
+					}
+				});
+			});
+
+			it('should limit the list to the `count` argument', async () => {
+				return new Promise(async (resolve, reject) => {
+					const timeseriesList = await indicators.listTimeseries('indicator1', 0, 1).catch(reject);
+					try {
+						assert.strictEqual(timeseriesList.list.length, 1);
+						resolve();
+					} catch (e) {
+						reject(e);
+					}
+				});
+			});
+
+			describe('Filtering', () => {
+				it.skip('should send filtering options to the timeseries `list` function');
+			});
+		});
+
+		describe('`getJsonStat` function', () => {
+			it.skip('should return a JSONStat object for the indicator', async () => {
+				// return new Promise(async (resolve, reject) => {
+				// 	const jsonstat = await indicators.getJsonStat('indicator1').catch(reject);
+				// 	try {
+				// 		assert.strictEqual();
+				// 		resolve();
+				// 	} catch (e) {
+				// 		reject(e);
+				// 	}
+				// });
+			});
+		});
+
+		describe('`getSDMX` function', () => {
+			it.skip('should return an SDMX string for the indicator', async () => {
+				// return new Promise(async (resolve, reject) => {
+				// 	const sdmx = await indicators.getSDMX('indicator1').catch(reject);
+				// 	try {
+				// 		assert.strictEqual();
+				// 		resolve();
+				// 	} catch (e) {
+				// 		reject(e);
+				// 	}
+				// });
+			});
+		});
+	});
+
+	describe('API', () => {
+		describe('Output', () => {
+
+		});
+
+		describe('Routes', () => {
+
+		});
+	});
+});
